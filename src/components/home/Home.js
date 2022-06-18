92cedec4d02d7333b62b23a2907fdc11446c85a0
ShaakhDev <shohbobolov98@gmail.com>
2022-06-19 01:44:14 +0500

    useffect changed

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -6,7 +6,6 @@ import Table from "./Table";
 
 function Home(props) {
    const navigate = useNavigate()
-   const [fetched, setFetched] = useState(false)
    const [users, setUsers] = useState([]);
    const currentUser = window.localStorage.getItem('_uid')
    const [checkedIds, setCheckedIds] = useState([])
@@ -15,16 +14,12 @@ function Home(props) {
       (async () => {
          const data = await GetAllUsers(navigate)
          setUsers(data?.users)
-         setFetched(true)
-         setTimeout(() => {
-            setFetched(false)
-         }, 1000);
       })()
-   }, [fetched])
+   }, [])
 
    return (
       <>
-         <Toolbar refresh={setFetched} checkedIds={checkedIds} currentUser={currentUser} />
+         <Toolbar checkedIds={checkedIds} currentUser={currentUser} />
          <Table callback={(i) => setCheckedIds(i)} data={users} />
       </>
    )
