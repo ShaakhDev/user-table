fe20e68b739381af1a59151c05c108b131b13956
ShaakhDev <shohbobolov98@gmail.com>
2022-06-19 01:57:40 +0500

    useEffect works only fetch

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -6,20 +6,24 @@ import Table from "./Table";
 
 function Home(props) {
    const navigate = useNavigate()
+   const [fetched, setFetched] = useState(false)
    const [users, setUsers] = useState([]);
    const currentUser = window.localStorage.getItem('_uid')
    const [checkedIds, setCheckedIds] = useState([])
 
    useEffect(() => {
-      (async () => {
-         const data = await GetAllUsers(navigate)
-         setUsers(data?.users)
-      })()
-   }, [users, navigate])
+      if (fetched) {
+         (async () => {
+            const data = await GetAllUsers(navigate)
+            setUsers(data?.users);
+            setFetched(false)
+         })()
+      }
+   }, [fetched])
 
    return (
       <>
-         <Toolbar checkedIds={checkedIds} currentUser={currentUser} />
+         <Toolbar setFetched={setFetched} checkedIds={checkedIds} currentUser={currentUser} />
          <Table callback={(i) => setCheckedIds(i)} data={users} />
       </>
    )
